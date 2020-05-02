b7926af74e7eed4705125d1b18323f011d3b2e5c
christoph murauer <chris.murauer@yahoo.de>
2020-05-02 15:28:53 +0200

    data folder

---
+++ movie_drone_simulator_Data/StreamingAssets/VRCapture/Demo/Scripts/RotationAround.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RotationAround : MonoBehaviour {
+    public GameObject centerPoint;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        this.transform.RotateAround(centerPoint.transform.position, Vector3.up, 100 * Time.deltaTime);
+    }
+}
